c45b7033b1520150b26fda81977258b9f4b6dfe3
jgtenbarge <163669548+jgtenbarge@users.noreply.github.com>
2024-03-18 15:06:32 -0600

    Update text-adventure.js

--- text-adventure.js
+++ text-adventure.js
@@ -25,7 +25,7 @@ function firstChoice() {
 // function that runs once you enter the cave
 function enterCave() {
   let choice = prompt(
-    "You enter the cave. It's a massive, damp, bowl shaped room. You can 'read' a sign in the center. There's also a tunnel to the 'east' and a tunnel to the 'west'."
+    "You enter the cave. It's a massive, damp, bowl-shaped room. You can 'read' a sign in the center. There's also a tunnel to the 'east' and a tunnel to the 'west'."
   ).toLowerCase();
   if (choice === null) {
     alert("Game aborted. Loser.");
@@ -57,7 +57,7 @@ function westTunnel() {
     westTunnel();
   } else if (choice === "run") {
     alert(
-      "You break into a dead sprint back the way you came! The rocks barely missing you as the tunnel crumbles around you! You run so fast you dart straight into the east tunnel!"
+      "You break into a dead sprint back the way you came! The rocks barely miss you as the tunnel crumbles around you! You run so fast you dart straight into the east tunnel!"
     );
     eastTunnel();
   } else if (choice === "face") {
@@ -103,7 +103,7 @@ function traps() {
       "You carefully navigate the unnatural stones. Delicately dancing from safe space to safe space and make your way to where the tunnel continues! You begin walking down the tunnel."
     );
     alert(
-      "The tunnel continues even deeper into the belly of the cave. At the end of the trail it opens to the largest underground cave you've ever seen! You stand on the edge of a cliff as the cave stretches for miles and miles and miles."
+      "The tunnel continues even deeper into the belly of the cave. At the end of the trail, it opens to the largest underground cave you've ever seen! You stand on the edge of a cliff as the cave stretches for miles and miles and miles."
     );
     alert(
       "The cave is dark but sunlight seeping through cracks in the ceiling slightly illuminates the cave, but suddenly the light is blocked as a massive figure covers the cracks. You look up to see a gigantic dragon fly across the cave!!"
@@ -111,7 +111,7 @@ function traps() {
     eastTunnelPrt2();
   } else if (choice === "dash") {
     alert(
-      "You begin sprinting through the opening, but with your speed you clumsly slam your foot into one of the false stones! You look up just in time to see a massive boulder fall from the ceiling crushing you! YOU DIED!!"
+      "You begin sprinting through the opening, but with your speed, you clumsily slam your foot into one of the false stones! You look up just in time to see a massive boulder fall from the ceiling crushing you! YOU DIED!!"
     );
     return;
   }
@@ -128,22 +128,24 @@ function eastTunnelPrt2() {
     return eastTunnelPrt2();
   } else if (choice === "watch") {
     alert(
-      "You watch the dragon but the dragon also watches you! Faster than you can react the massive beast whips around and dive bombs directly at you! In one massive chomp the dragon eats half of the cliff side and you with it. YOU DIED!!"
+      "You watch the dragon but the dragon also watches you! Faster than you can react the massive beast whips around and dives bombs directly at you! In one massive chomp the dragon eats half of the cliffside and you with it. YOU DIED!!"
     );
     return;
   } else if (choice === "hide") {
     alert(
-      "You dart back into the tunnel and hide against the wall. Perfect timing because the dragon spins around and begins heading for exactly where you were standing! It takes a massive chomp out of the cliff side!"
+      "You dart back into the tunnel and hide against the wall. Perfect timing because the dragon spins around and begins heading for exactly where you were standing! It takes a massive chomp out of the cliffside!"
     );
     alert(
       "The dragon heads towards the back of the cave where it lands. It curls into a scaley ball and seems to fall asleep."
     );
     alert(
-      "The chomp the dragon left creates a rocky ramp down to the cave floor. You slide down and see the dragon asleep a mere 200 feet infront of you!"
+      "The chomp the dragon left creates a rocky ramp down to the cave floor. You slide down and see the dragon asleep a mere 200 feet in front of you!"
     );
     caveFloor();
   }
 }
 
 // function for cave floor
-function caveFloor() {}
+function caveFloor() {
+  
+}
